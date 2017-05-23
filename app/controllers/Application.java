8114f109427c6c7d8048c7a346c747e91223dc5c
khatanaka9 <hatanaka@inazou.jp>
2017-05-23 19:12:27 +0900

    yabe コメント表示

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,16 +4,17 @@ import java.util.*;
 
 import models.*;
 import play.*;
-import play.data.binding.*;
+import play.data.validation.*;
 import play.mvc.*;
 
 public class Application extends Controller {
 
-
 	@Before
 	static void addDefaults() {
-	    renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
-	    renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+		renderArgs.put("blogTitle",
+				Play.configuration.getProperty("blog.title"));
+		renderArgs.put("blogBaseline",
+				Play.configuration.getProperty("blog.baseline"));
 	}
 
 	public static void index() {
@@ -23,4 +24,19 @@ public class Application extends Controller {
 		render(frontPost, olderPosts);
 	}
 
+	public static void show(Long id) {
+		Post post = Post.findById(id);
+		render(post);
+	}
+
+	public static void postComment(Long postId, @Required String author,
+			@Required String content) {
+		Post post = Post.findById(postId);
+		if(validation.hasErrors()) {
+			render("Application/show.html",post);
+		}
+		post.addComment(author, content);
+		flash.success("Thanks for posting %s", author);
+		show(postId);
+	}
 }
